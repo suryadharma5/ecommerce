b9a7169ede04364a4850548a8744818cfabeeccb
suryadharma5 <suryadharmasetiawan03@gmail.com>
2026-04-01 15:30:08 +0700

    added: customer data models

---
+++ services/customer-service/src/main/java/com/sdh/ecommerce/models/CustomerRequest.java
@@ -0,0 +1,23 @@
+package com.sdh.ecommerce.models;
+
+import com.sdh.ecommerce.entity.Address;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+
+public record CustomerRequest(
+        String id,
+
+        @NotNull(message = "Customer firstname is required")
+        String firstName,
+
+        @NotNull(message = "Customer email is required")
+        @Email(message = "Customer email is not a valid email address")
+        String email,
+
+        @NotNull(message = "Customer lastname is required")
+        String lastName,
+
+        Address address
+) {
+
+}
---
+++ services/customer-service/src/main/java/com/sdh/ecommerce/models/CustomerResponse.java
@@ -0,0 +1,19 @@
+package com.sdh.ecommerce.models;
+
+import com.sdh.ecommerce.entity.Address;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CustomerResponse {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Address address;
+}
