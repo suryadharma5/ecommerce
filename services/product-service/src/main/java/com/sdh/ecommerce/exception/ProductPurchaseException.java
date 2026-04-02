7485d4c76b6c2083949e9639bef82fb1c778fb17
suryadharma5 <suryadharmasetiawan03@gmail.com>
2026-04-02 14:14:40 +0700

    feat: add Exception for product exception

---
+++ services/product-service/src/main/java/com/sdh/ecommerce/exception/ProductPurchaseException.java
@@ -0,0 +1,7 @@
+package com.sdh.ecommerce.exception;
+
+public class ProductPurchaseException extends RuntimeException {
+    public ProductPurchaseException(String s) {
+        super(s);
+    }
+}
