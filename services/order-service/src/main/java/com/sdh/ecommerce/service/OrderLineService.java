94a403b84a154d8455aadecef6691d3080be2438
suryadharma5 <suryadharmasetiawan03@gmail.com>
2026-04-07 14:55:57 +0700

    feat: add findAllByOrderId method in orderline service

--- services/order-service/src/main/java/com/sdh/ecommerce/service/OrderLineService.java
+++ services/order-service/src/main/java/com/sdh/ecommerce/service/OrderLineService.java
@@ -2,9 +2,14 @@ package com.sdh.ecommerce.service;
 
 import com.sdh.ecommerce.mapper.OrderLineMapper;
 import com.sdh.ecommerce.models.OrderLineRequest;
+import com.sdh.ecommerce.models.OrderLineResponse;
 import com.sdh.ecommerce.repository.OrderLineRepository;
 import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.Nullable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -15,5 +20,12 @@ public class OrderLineService {
     public Integer saveOrderLine(OrderLineRequest request) {
         var order = mapper.toOrderLine(request);
         return repository.save(order).getId();
+    }
+
+    public List<OrderLineResponse> findAllByOrderId(Integer orderId) {
+        return repository.findAllByOrderId(orderId)
+                .stream()
+                .map(mapper::toOrderLineResponse)
+                .collect(Collectors.toList());
     }
 }
