b447836fd67d25860050675a4e2027b3068961d7
suryadharma5 <suryadharmasetiawan03@gmail.com>
2026-04-07 17:34:31 +0700

    feat: add notification consumer

---
+++ services/notification-service/src/main/java/com/sdh/ecommerce/kafka/NotificationConsumer.java
@@ -0,0 +1,67 @@
+package com.sdh.ecommerce.kafka;
+
+import com.sdh.ecommerce.email.EmailService;
+import com.sdh.ecommerce.entity.Notification;
+import com.sdh.ecommerce.entity.NotificationType;
+import com.sdh.ecommerce.kafka.models.OrderConfirmation;
+import com.sdh.ecommerce.kafka.models.PaymentConfirmation;
+import com.sdh.ecommerce.repository.NotificationRepository;
+import jakarta.mail.MessagingException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class NotificationConsumer {
+
+    private final NotificationRepository notificationRepository;
+    private final EmailService emailService;
+
+    @KafkaListener(topics = "payment-topic")
+    public void consumePaymentSuccessNotification(PaymentConfirmation paymentConfirmation) throws MessagingException {
+        log.info("Consuming message from payment-topic: {}", paymentConfirmation);
+        notificationRepository.save(
+                Notification.builder()
+                        .type(NotificationType.PAYMENT_CONFIRMATION)
+                        .notificationDate(LocalDateTime.now())
+                        .paymentConfirmation(paymentConfirmation)
+                        .build()
+        );
+
+        // send email
+        var customerName = paymentConfirmation.getCustomerFirstName() + " " + paymentConfirmation.getCustomerLastName();
+        emailService.sendPaymentSuccessEmail(
+                paymentConfirmation.getCustomerEmail(),
+                customerName,
+                paymentConfirmation.getAmount(),
+                paymentConfirmation.getOrderReference()
+        );
+    }
+
+    @KafkaListener(topics = "order-topic")
+    public void consumeOrderConfirmationNotification(OrderConfirmation orderConfirmation) throws MessagingException {
+        log.info("Consuming message from order-topic: {}", orderConfirmation);
+        notificationRepository.save(
+                Notification.builder()
+                        .type(NotificationType.ORDER_CONFIRMATION)
+                        .notificationDate(LocalDateTime.now())
+                        .orderConfirmation(orderConfirmation)
+                        .build()
+        );
+
+        // send email
+        var customerName = orderConfirmation.getCustomer().getFirstName() + " " + orderConfirmation.getCustomer().getLastName();
+        emailService.sendOrderConfirmationEmail(
+                orderConfirmation.getCustomer().getEmail(),
+                customerName,
+                orderConfirmation.getTotalAmount(),
+                orderConfirmation.getOrderReference(),
+                orderConfirmation.getProducts()
+        );
+    }
+}
