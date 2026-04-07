3149082dc3f8032a8ecce7c21ae87cd4eb020ee5
suryadharma5 <suryadharmasetiawan03@gmail.com>
2026-04-07 11:05:48 +0700

    chore: move files

--- services/order-service/src/main/java/com/sdh/ecommerce/config/RestTemplateConfig.java
+++ services/order-service/src/main/java/com/sdh/ecommerce/config/RestTemplateConfig.java
@@ -1,4 +1,4 @@
-package com.sdh.ecommerce.client;
+package com.sdh.ecommerce.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
