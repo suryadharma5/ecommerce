566ddb105146f780f2e84e481f2d812f3b1bed4e
suryadharma5 <suryadharmasetiawan03@gmail.com>
2026-04-07 14:54:20 +0700

    feat: add data mapper for order and order line

--- services/order-service/src/main/java/com/sdh/ecommerce/mapper/OrderLineMapper.java
+++ services/order-service/src/main/java/com/sdh/ecommerce/mapper/OrderLineMapper.java
@@ -3,6 +3,7 @@ package com.sdh.ecommerce.mapper;
 import com.sdh.ecommerce.entity.Order;
 import com.sdh.ecommerce.entity.OrderLine;
 import com.sdh.ecommerce.models.OrderLineRequest;
+import com.sdh.ecommerce.models.OrderLineResponse;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,6 +18,13 @@ public class OrderLineMapper {
                                 .build()
                 )
                 .productId(request.getProductId())
+                .build();
+    }
+
+    public OrderLineResponse toOrderLineResponse(OrderLine orderLine) {
+        return OrderLineResponse.builder()
+                .id(orderLine.getId())
+                .quantity(orderLine.getQuantity())
                 .build();
     }
 }
--- services/order-service/src/main/java/com/sdh/ecommerce/mapper/OrderMapper.java
+++ services/order-service/src/main/java/com/sdh/ecommerce/mapper/OrderMapper.java
@@ -2,6 +2,7 @@ package com.sdh.ecommerce.mapper;
 
 import com.sdh.ecommerce.entity.Order;
 import com.sdh.ecommerce.models.OrderRequest;
+import com.sdh.ecommerce.models.OrderResponse;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,6 +16,16 @@ public class OrderMapper {
                 .reference(request.getReference())
                 .totalAmount(request.getAmount())
                 .paymentMethod(request.getPaymentMethod())
+                .build();
+    }
+
+    public OrderResponse fromOrder(Order order) {
+        return OrderResponse.builder()
+                .id(order.getId())
+                .reference(order.getReference())
+                .customerId(order.getCustomerId())
+                .amount(order.getTotalAmount())
+                .paymentMethod(order.getPaymentMethod())
                 .build();
     }
 }
