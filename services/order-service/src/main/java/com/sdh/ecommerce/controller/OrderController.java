29d7a9788b662b6fa6ab0220d0f99c28a6ef5d48
suryadharma5 <suryadharmasetiawan03@gmail.com>
2026-04-07 14:54:06 +0700

    feat: add order and order line controllers

--- services/order-service/src/main/java/com/sdh/ecommerce/controller/OrderController.java
+++ services/order-service/src/main/java/com/sdh/ecommerce/controller/OrderController.java
@@ -1,14 +1,14 @@
 package com.sdh.ecommerce.controller;
 
 import com.sdh.ecommerce.models.OrderRequest;
+import com.sdh.ecommerce.models.OrderResponse;
 import com.sdh.ecommerce.service.OrderService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,5 +20,15 @@ public class OrderController {
     @PostMapping
     public ResponseEntity<Integer> createOrder(@RequestBody @Valid OrderRequest order) {
         return ResponseEntity.ok(orderService.create(order));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<OrderResponse>> findAll() {
+        return ResponseEntity.ok(orderService.findAll());
+    }
+
+    @GetMapping("/{orderId}")
+    public ResponseEntity<OrderResponse> findById(@PathVariable("orderId") Integer orderId) {
+        return ResponseEntity.ok(orderService.findById(orderId));
     }
 }
---
+++ services/order-service/src/main/java/com/sdh/ecommerce/controller/OrderLineController.java
@@ -0,0 +1,26 @@
+package com.sdh.ecommerce.controller;
+
+import com.sdh.ecommerce.entity.OrderLine;
+import com.sdh.ecommerce.models.OrderLineResponse;
+import com.sdh.ecommerce.service.OrderLineService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/order-lines")
+@RequiredArgsConstructor
+public class OrderLineController {
+
+    private final OrderLineService orderLineService;
+
+    @GetMapping("/order/{orderId}")
+    public ResponseEntity<List<OrderLineResponse>> getOrderLineByOrderId(@PathVariable("orderId") Integer orderId) {
+        return ResponseEntity.ok(orderLineService.findAllByOrderId(orderId));
+    }
+}
