bac058caec1c513535f53e1a6be79d1dbfd9dfde
suryadharma5 <suryadharmasetiawan03@gmail.com>
2026-04-07 14:54:51 +0700

    feat: add find order line by order id method

--- services/order-service/src/main/java/com/sdh/ecommerce/repository/OrderLineRepository.java
+++ services/order-service/src/main/java/com/sdh/ecommerce/repository/OrderLineRepository.java
@@ -3,5 +3,8 @@ package com.sdh.ecommerce.repository;
 import com.sdh.ecommerce.entity.OrderLine;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderLineRepository extends JpaRepository<OrderLine, Integer> {
+    List<OrderLine> findAllByOrderId(Integer orderId);
 }
